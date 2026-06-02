8cb72e6d52b2eaa6811452baf35ec741ad242edc
praneemamidi04042005 <praneeth.23bce9683@vitapstudent.ac.in>
2026-06-02 15:17:42 +0530

    Time: 1 ms (99.93%) | Memory: 94.3 MB (82.66%) - LeetSync

---
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int min_price=Integer.MAX_VALUE;
+        int max_price=0;
+        for(int price:prices){
+            min_price=Math.min(min_price,price);
+            max_price=Math.max(max_price,price-min_price);
+        }
+        return max_price;
+    }
+}
