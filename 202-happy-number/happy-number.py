7595bbc498c051d59444aeb506e1c4db78947116
praneemamidi04042005 <praneeth.23bce9683@vitapstudent.ac.in>
2026-08-03 16:12:01 +0530

    Time: 3 ms (33.13%) | Memory: 19.3 MB (24.18%) - LeetSync

---
+++ 202-happy-number/happy-number.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        if n==1 or n==7:
+            return True
+        elif n<10:
+            return False
+        else:
+            s=0
+            while n>0:
+                s+=(n%10)*(n%10)
+                n//=10
+            return self.isHappy(s)
+            
+
+        
