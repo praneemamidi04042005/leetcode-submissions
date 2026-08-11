96a201097818ab4a56f1dcf297b6630f76d398d2
praneemamidi04042005 <praneeth.23bce9683@vitapstudent.ac.in>
2026-08-11 16:38:02 +0530

    Time: 281 ms (91.14%) | Memory: 0B (100.00%) - LeetSync

---
+++ 4195-find-users-with-high-token-usage/find-users-with-high-token-usage.sql
@@ -0,0 +1,2 @@
+
+select user_id,count(*) as prompt_count,round(avg(tokens),2) as avg_tokens from prompts group by user_id having count(*)>2 and max(tokens)>avg(tokens) order by avg_tokens desc,user_id;
